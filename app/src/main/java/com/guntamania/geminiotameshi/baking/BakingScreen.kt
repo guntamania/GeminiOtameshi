0f93d65f8ffb49fb41a47c8679c36935cf466401
guntama <medaka.mp3@gmail.com>
2025-06-08 16:00:57 +0900

    Add action settings

--- app/src/main/java/com/guntamania/geminiotameshi/baking/BakingScreen.kt
+++ app/src/main/java/com/guntamania/geminiotameshi/baking/BakingScreen.kt
@@ -8,12 +8,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -34,6 +40,7 @@ import androidx.navigation.compose.rememberNavController
 import com.guntamania.geminiotameshi.R
 import java.util.Date
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BakingScreen(
     navController: NavController,
@@ -45,78 +52,97 @@ fun BakingScreen(
     val uiState by bakingViewModel.uiState.collectAsState()
     val context = LocalContext.current
 
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(
-            text = stringResource(R.string.baking_title),
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(16.dp)
-        )
-
-        Row(
-            modifier = Modifier.padding(all = 16.dp)
-        ) {
-            TextField(
-                value = prompt,
-                label = { Text(stringResource(R.string.label_prompt)) },
-                onValueChange = { prompt = it },
-                modifier = Modifier
-                    .weight(0.8f)
-                    .padding(end = 16.dp)
-                    .align(Alignment.CenterVertically)
-            )
-
-            Button(
-                onClick = {
-                    bakingViewModel.sendPrompt(prompt)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.baking_title),
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.padding(16.dp)
+                    )
                 },
-                enabled = prompt.isNotEmpty(),
-                modifier = Modifier
-                    .align(Alignment.CenterVertically)
-            ) {
-                Text(text = stringResource(R.string.action_go))
-            }
-        }
-
-        if (uiState is BakingUiState.Loading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
-        } else {
-            var textColor = MaterialTheme.colorScheme.onSurface
-            val messages =
-                when (uiState) {
-                    is BakingUiState.Error -> {
-                        textColor = MaterialTheme.colorScheme.error
-                        listOf(
-                            BakingViewData.Entry(
-                                date = Date(),
-                                message = (uiState as BakingUiState.Error).errorMessage
-                            )
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Settings,
+                            contentDescription = "Localized description"
                         )
                     }
+                },
+            )
+        },
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding).fillMaxSize()
+        ) {
 
-                    is BakingUiState.Success -> {
-                        textColor = MaterialTheme.colorScheme.onSurface
-                        (uiState as BakingUiState.Success).data.messages
-                    }
+            Row(
+                modifier = Modifier.padding(all = 16.dp)
+            ) {
+                TextField(
+                    value = prompt,
+                    label = { Text(stringResource(R.string.label_prompt)) },
+                    onValueChange = { prompt = it },
+                    modifier = Modifier
+                        .weight(0.8f)
+                        .padding(end = 16.dp)
+                        .align(Alignment.CenterVertically)
+                )
 
-                    else -> {
-                        emptyList()
-                    }
+                Button(
+                    onClick = {
+                        bakingViewModel.sendPrompt(prompt)
+                    },
+                    enabled = prompt.isNotEmpty(),
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                ) {
+                    Text(text = stringResource(R.string.action_go))
                 }
+            }
 
-            val scrollState = rememberScrollState()
+            if (uiState is BakingUiState.Loading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+            } else {
+                var textColor = MaterialTheme.colorScheme.onSurface
+                val messages =
+                    when (uiState) {
+                        is BakingUiState.Error -> {
+                            textColor = MaterialTheme.colorScheme.error
+                            listOf(
+                                BakingViewData.Entry(
+                                    date = Date(),
+                                    message = (uiState as BakingUiState.Error).errorMessage
+                                )
+                            )
+                        }
 
-            LazyColumn( Modifier.fillMaxSize() ) {
-                items(messages.size) {
-                    ListItem(
-                        result = messages.get(it),
-                        scrollState = scrollState,
-                        textColor = textColor
-                    )
+                        is BakingUiState.Success -> {
+                            textColor = MaterialTheme.colorScheme.onSurface
+                            (uiState as BakingUiState.Success).data.messages
+                        }
+
+                        else -> {
+                            emptyList()
+                        }
+                    }
+
+                val scrollState = rememberScrollState()
+
+                LazyColumn(Modifier.fillMaxSize()) {
+                    items(messages.size) {
+                        ListItem(
+                            result = messages.get(it),
+                            scrollState = scrollState,
+                            textColor = textColor
+                        )
+                    }
                 }
             }
         }
+
     }
 }
 
